b67b14cc7bd8a53559d4e73cb1086cfc0be85312
Admin <mucahitozcantr16@gmail.com>
2021-05-16 13:12:21 +0300

    mucahit
    Mucahit , bos-chat
    
    Kullanılan teknolojiler
    
    React js , redux.
    
    back end: chatengine

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed.jsx';
+import LoginForm from './components/LoginForm'
+import LOGOUT from './components/Logout'
+
+import './App.css';
+
+const projectID = '88ec11aa-ea55-49f8-ba5a-abe08ee3259e';
+
+const App = () => {
+  if (!localStorage.getItem('username')) return  <LoginForm/>   ;
+
+  return (
+   
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+ 
+    );
+};
+
+
+export default App;
---
+++ src/components/Logout.jsx
@@ -0,0 +1,37 @@
+import { Component } from "react";
+
+
+
+class LOGOUT extends Component {
+    constructor(props){
+            super(props);
+            this.lgout= this.lgout.bind(this)
+    }
+    lgout(){
+       localStorage.clear();
+        window.location.reload();
+    }
+
+    render(){
+    return (   
+        
+        <div>
+
+      <button type="button"
+       onClick={this.lgout}
+    style={{display:'flex', position: 'fixed',borderRadius:'360px'}}
+    class="btn btn-outline-primary    ml-1 mt-1   mb-6"
+        
+        >Çıkış yap</button>
+
+            </div>
+
+        )
+    }
+}
+export default LOGOUT;
+
+              
+            
+
+
